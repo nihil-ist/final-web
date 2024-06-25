82c82be7b7c33ebb206682a9924de56658e78fe2
CocoAsombroso <Ivanelmen420@gmail.com>
2024-06-25 00:28:14 -0600

    ahora si bien

--- angular/src/app/app.routes.ts
+++ angular/src/app/app.routes.ts
@@ -6,15 +6,8 @@ import { GalleryComponent } from './gallery/gallery.component';
 import { SingleApartmentComponent } from './single-apartment/single-apartment.component';
 import { TableComponent } from './table/table.component';
 import { SignuppComponent } from './signupp/signupp.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { ContactComponent } from './contact/contact.component';
-=======
 import { LoginnComponent } from './loginn/loginn.component';
->>>>>>> be4157dc0693a6decbafb25b3a735615fe2d7583
-=======
-import { LoginnComponent } from './loginn/loginn.component';
->>>>>>> be4157dc0693a6decbafb25b3a735615fe2d7583
 
 export const routes: Routes = [
     {path: 'home', component: HomeComponent},
@@ -24,14 +17,7 @@ export const routes: Routes = [
     {path: 'apartment/:id', component: SingleApartmentComponent},
     {path: 'reservations', component: TableComponent},
     {path: 'register', component: SignuppComponent},
-<<<<<<< HEAD
-<<<<<<< HEAD
     {path: 'contact', component: ContactComponent},
-=======
     {path: 'signin', component: LoginnComponent},
->>>>>>> be4157dc0693a6decbafb25b3a735615fe2d7583
-=======
-    {path: 'signin', component: LoginnComponent},
->>>>>>> be4157dc0693a6decbafb25b3a735615fe2d7583
     {path: '**', pathMatch: 'full', redirectTo: 'home'}
 ];
