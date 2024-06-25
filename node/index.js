d63e9d1ff2497baf8d611e2a4d4e80da6e9a018f
CocoAsombroso <Ivanelmen420@gmail.com>
2024-06-24 19:15:42 -0600

    Parte de node agregada (kindof)

---
+++ node/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const misrutas = require('./routes/rutas');
+const cors = require('cors')
+
+const app = express(); 
+const port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended:false}));
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/',misrutas);
+
+
+app.listen(port, () => {
+ console.log(`hola servidor ejecucion en http://localhost:${port}`);
+});
