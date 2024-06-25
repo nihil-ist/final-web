cdf0e74f22c29a912f845d073f49390be11f0b93
Diego Zamora Delgadillo <al261855@edu.uaa.mx>
2024-06-25 01:42:23 -0600

    sign up mejor

--- angular/src/app/signupp/signupp.component.ts
+++ angular/src/app/signupp/signupp.component.ts
@@ -10,32 +10,59 @@ import { AuthService } from '../services/auth.service';
 import { NavbarComponent } from '../navbar/navbar.component';
 import {MatButtonModule} from '@angular/material/button'; 
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { passwordMismatchValidator } from '../password-mismatch.validator';
 
 @Component({
   selector: 'app-signupp',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule, NavbarComponent, MatButtonModule, RouterModule],
+  imports: [ReactiveFormsModule, FormsModule, NavbarComponent, MatButtonModule, RouterModule, CommonModule],
   templateUrl: './signupp.component.html',
   styleUrl: './signupp.component.css',
 })
 export class SignuppComponent {
   constructor(private fb: FormBuilder, private auth: AuthService) {}
 
+  errorMessage: string | null = null;
+
   form: FormGroup = this.fb.group({
     email: ['', Validators.required],
-    username: ['', Validators.required],
     password: ['', Validators.required],
-  });
+    confirmPassword: ['', [Validators.required, Validators.minLength(6)]],
+    fullname: ['', Validators.required],
+    username: ['', Validators.required],
+  }, { validators: passwordMismatchValidator });
 
   onSubmit(): void {
+
+    this.errorMessage = null;
+    if  (this.form.invalid) {
+      return;
+    }
+
     const rawFrom = this.form.getRawValue();
     this.auth
-      .register(rawFrom.email, rawFrom.username, rawFrom.password)
+      .register(rawFrom.email, rawFrom.password, rawFrom.fullname, rawFrom.username)
       .subscribe({
-        next: () => {},
+        next: () => {
+          console.log('User registered successfully');
+        },
         error: (err) => {
+          this.handleError(err);
           console.error('Error:', err);
         },
       });
   }
+
+  private handleError(error: any): void {
+    if(error.code === 'auth/email-already-in-use') {
+      this.errorMessage = 'This email address is already in use';
+    } else if (error.code === 'auth/weak-password') {
+      this.errorMessage = 'Password must be at least 6 characters long';
+    } else {
+      this.errorMessage = 'An error ocurred. Please try again.';
+    }
+    console.error('Error', error);
+  }
+
 }
