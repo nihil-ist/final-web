2d57527ddaecc42cf9525c24209e1eae5d6aa7a8
Valeria <ppollux.velas@gmail.com>
2024-06-26 03:39:40 -0600

    no

--- node/index.js
+++ node/index.js
@@ -4,6 +4,7 @@ const misrutas = require('./routes/rutas');
 const cors = require('cors')
 const db = require('./firebase-config');
 
+
 const app = express(); 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended:false}));
@@ -15,3 +16,7 @@ app.use('/',misrutas);
 app.listen(port, () => {
  console.log(`hola servidor ejecucion en http://localhost:${port}`);
 });
+app.use(cors({
+    origin: 'http://localhost:4200',
+    credentials: true  // Si estás usando credenciales en las peticiones (cookies, headers personalizados, etc.)
+  }));
