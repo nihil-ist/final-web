91d231c4601ce17d9af180ee48899543c9fe12e5
Rodolfo Puente Zuñiga <rjunior007@live.com.mx>
2024-06-25 21:27:03 -0600

    qr parte 1

--- angular/src/app/qr-generator/qr-generator.component.ts
+++ angular/src/app/qr-generator/qr-generator.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from '../services/firebase-service.service';
+import { Reservation } from '../models/reservation.model';
+import { Observable } from 'rxjs';
+import { QRCodeModule } from 'angularx-qrcode';
 
 @Component({
   selector: 'app-qr-generator',
   standalone: true,
-  imports: [],
+  imports: [QRCodeModule],
   templateUrl: './qr-generator.component.html',
   styleUrl: './qr-generator.component.css'
 })
-export class QrGeneratorComponent {
+export class QrGeneratorComponent implements OnInit {
+  reservations$:  Observable<Reservation[]>;
+  selectedReservation: Reservation | null = null;
+  qrData: string = '';
 
+  constructor(private firebaseService: FirebaseService) {
+    this.reservations$ = this.firebaseService.getAll().valueChanges();
+  }
+
+  ngOnInit(): void {
+  }
+
+  generateQrCode(reservation: Reservation): void {
+    this.selectedReservation = reservation;
+    this.qrData = JSON.stringify(reservation);
+  }
 }
